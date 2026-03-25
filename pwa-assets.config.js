7f8ad940cba78e56184ed5990b441237a604a68e
허미숙 <altnr23@naver.com>
2026-03-25 14:35:30 +0900

    style: remove padding from PWA generated icons

--- pwa-assets.config.js
+++ pwa-assets.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  preset: 'minimal-2023',
-  images: ['public/icon.png'],
+  preset: {
+    transparent: {
+      sizes: [64, 192, 512],
+      favicons: [[48, 'favicon.ico']]
+    },
+    maskable: {
+      sizes: [512],
+      padding: 0
+    },
+    apple: {
+      sizes: [180],
+      padding: 0
+    }
+  },
+  images: ['public/icon.png']
 })
